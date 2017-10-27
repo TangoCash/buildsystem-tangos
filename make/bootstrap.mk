7e7a53269026bd1ca73c8e2cdd56cdd280e74bda
TangoCash <eric@loxat.de>
2017-10-27 13:16:32 +0200

    fix hd51

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -240,9 +240,7 @@ $(D)/bootstrap: $(BOOTSTRAP)
 SYSTEM_TOOLS  = $(D)/busybox
 SYSTEM_TOOLS += $(D)/zlib
 SYSTEM_TOOLS += $(D)/sysvinit
-ifeq ($(BOXARCH), sh4)
 SYSTEM_TOOLS += $(D)/diverse-tools
-endif
 SYSTEM_TOOLS += $(D)/e2fsprogs
 SYSTEM_TOOLS += $(D)/jfsutils
 SYSTEM_TOOLS += $(D)/hdidle
