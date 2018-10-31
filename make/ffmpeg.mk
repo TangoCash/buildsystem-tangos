2621a277a0443efc9a17914c30d12e728fd872d0
TangoCash <eric@loxat.de>
2018-10-31 23:14:22 +0100

    disable older ffmpeg for arm

--- make/ffmpeg.mk
+++ make/ffmpeg.mk
@@ -4,7 +4,7 @@
 ################################################################################
 ifeq ($(BOXARCH), arm)
 
-ifeq ($(FFMPEG_EXPERIMENTAL), 1)
+#ifeq ($(FFMPEG_EXPERIMENTAL), 1)
 FFMPEG_VER = 4.0.2
 FFMPEG_PATCH = ffmpeg-$(FFMPEG_VER)-aac.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-allow_to_choose_rtmp_impl_at_runtime.patch
@@ -14,29 +14,29 @@ FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix-hls.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix_mpegts.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-hls_replace_key_uri.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-mips64_cpu_detection.patch
-else
-FFMPEG_VER = 3.3
-FFMPEG_PATCH = ffmpeg-$(FFMPEG_VER)-aac.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-add_dash_demux.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-allow_to_choose_rtmp_impl_at_runtime.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-buffer-size.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-chunked_transfer_fix_eof.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix-edit-list-parsing.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix-hls.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix_mpegts.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-hls_replace_key_uri.patch
-endif
+#else
+#FFMPEG_VER = 3.3
+#FFMPEG_PATCH = ffmpeg-$(FFMPEG_VER)-aac.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-add_dash_demux.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-allow_to_choose_rtmp_impl_at_runtime.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-buffer-size.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-chunked_transfer_fix_eof.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix-edit-list-parsing.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix-hls.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix_mpegts.patch
+#FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-hls_replace_key_uri.patch
+#endif
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VER).tar.xz
 
 FFMPEG_DEPS = $(D)/librtmp
 FFMPEG_CONF_OPTS  = --enable-librtmp --enable-openssl
-ifeq ($(FFMPEG_EXPERIMENTAL), 1)
+#ifeq ($(FFMPEG_EXPERIMENTAL), 1)
 FFMPEG_CONF_OPTS  += --enable-libxml2
 FFMPEG_CONF_OPTS  += --disable-x86asm
-else
-FFMPEG_CONF_OPTS  += --disable-yasm
-FFMPEG_CONF_OPTS  += --disable-ffserver
-endif
+#else
+#FFMPEG_CONF_OPTS  += --disable-yasm
+#FFMPEG_CONF_OPTS  += --disable-ffserver
+#endif
 FFMPRG_EXTRA_CFLAGS  = -I$(TARGET_DIR)/usr/include/libxml2
 
 $(ARCHIVE)/$(FFMPEG_SOURCE):
