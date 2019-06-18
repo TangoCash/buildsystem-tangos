Duckbox-Developers/buildsystem-ddt

rename apps -> tools

Makefile
@@ -30,7 +30,7 @@ printenv:
 	@echo "ARCHIVE_DIR       : $(ARCHIVE)"
 	@echo "BASE_DIR          : $(BASE_DIR)"
 	@echo "CUSTOM_DIR        : $(CUSTOM_DIR)"
-	@echo "APPS_DIR          : $(APPS_DIR)"
+	@echo "TOOLS_DIR         : $(TOOLS_DIR)"
 	@echo "DRIVER_DIR        : $(DRIVER_DIR)"
 	@echo "FLASH_DIR         : $(FLASH_DIR)"
 	@echo "CROSS_DIR         : $(CROSS_DIR)"
@@ -78,7 +78,7 @@ help:
 	@echo ""
 	@echo "later, you might find these useful:"
 	@echo "* make update-self         - update the build system"
-	@echo "* make update              - update the build system, apps, driver and flash"
+	@echo "* make update              - update the build system, tools, driver and flash"
 	@echo ""
 	@echo "cleantargets:"
 	@echo "make clean                 - Clears everything except kernel."
@@ -147,14 +147,14 @@ update:
 		fi; \
 	fi
 	@echo;
-	@if test -d $(APPS_DIR); then \
-		cd $(APPS_DIR)/; \
+	@if test -d $(TOOLS_DIR); then \
+		cd $(TOOLS_DIR)/; \
 		echo '==================================================================='; \
-		echo '      updating $(GIT_NAME_APPS)-apps git repository'; \
+		echo '      updating $(GIT_NAME_TOOLS)-tools git repository'; \
 		echo '==================================================================='; \
 		echo; \
 		if [ "$(GIT_STASH_PULL)" = "stashpull" ]; then \
-			git stash && git stash show -p > ./pull-stash-apps.patch || true && git pull && git stash pop || true; \
+			git stash && git stash show -p > ./pull-stash-tools.patch || true && git pull && git stash pop || true; \
 		else \
 			git pull; \
 		fi; \

make/bootstrap.mk
@@ -285,12 +285,12 @@ $(DRIVER_DIR):
 		git clone $(GITHUB)/$(GIT_NAME_DRIVER)/driver.git driver; \
 	fi
 
-$(APPS_DIR):
+$(TOOLS_DIR):
 	@echo '===================================================================='
-	@echo '      Cloning $(GIT_NAME_APPS)-apps git repository'
+	@echo '      Cloning $(GIT_NAME_TOOLS)-tools git repository'
 	@echo '===================================================================='
-	if [ ! -e $(APPS_DIR)/.git ]; then \
-		git clone $(GITHUB)/$(GIT_NAME_APPS)/apps.git apps; \
+	if [ ! -e $(TOOLS_DIR)/.git ]; then \
+		git clone $(GITHUB)/$(GIT_NAME_TOOLS)/tools.git tools; \
 	fi
 
 $(FLASH_DIR):
@@ -303,7 +303,7 @@ $(FLASH_DIR):
 	@echo ''
 
 PREQS  = $(DRIVER_DIR)
-PREQS += $(APPS_DIR)
+PREQS += $(TOOLS_DIR)
 PREQS += $(FLASH_DIR)
 
 preqs: $(PREQS)

TangoCash/buildsystem-tangos

Makefile
@@ -30,7 +30,7 @@ printenv:
 	@echo "ARCHIVE_DIR       : $(ARCHIVE)"
 	@echo "BASE_DIR          : $(BASE_DIR)"
 	@echo "CUSTOM_DIR        : $(CUSTOM_DIR)"
-	@echo "APPS_DIR          : $(APPS_DIR)"
+	@echo "TOOLS_DIR         : $(TOOLS_DIR)"
 	@echo "DRIVER_DIR        : $(DRIVER_DIR)"
 	@echo "FLASH_DIR         : $(FLASH_DIR)"
 	@echo "CROSS_DIR         : $(CROSS_DIR)"
@@ -97,7 +97,7 @@ help:
 	@echo ""
 	@echo "later, you might find these useful:"
 	@echo "* make update-self         - update the build system"
-	@echo "* make update              - update the build system, apps, driver and flash"
+	@echo "* make update              - update the build system, tools, driver and flash"
 	@echo ""
 	@echo "cleantargets:"
 	@echo "make clean                 - Clears everything except kernel."
@@ -168,14 +168,14 @@ update:
 		fi; \
 	fi
 	@echo;
-	@if test -d $(APPS_DIR); then \
-		cd $(APPS_DIR)/; \
+	@if test -d $(TOOLS_DIR); then \
+		cd $(TOOLS_DIR)/; \
 		$(call draw_line,); \
-		echo '      updating $(GIT_NAME_APPS)-apps git repository'; \
+		echo '      updating $(GIT_NAME_APPS)-tools git repository'; \
 		$(call draw_line,); \
 		echo; \
 		if [ "$(GIT_STASH_PULL)" = "stashpull" ]; then \
-			git stash && git stash show -p > ./pull-stash-apps.patch || true && git pull && git stash pop || true; \
+			git stash && git stash show -p > ./pull-stash-tools.patch || true && git pull && git stash pop || true; \
 		else \
 			git pull; \
 		fi; \

make/bootstrap.mk
@@ -351,12 +351,12 @@ $(DRIVER_DIR):
 		git clone $(GITHUB)/$(GIT_NAME_DRIVER)/driver.git driver; \
 	fi
 
-$(APPS_DIR):
+$(TOOLS_DIR):
 	$(call draw_line,)
-	@echo '      Cloning $(GIT_NAME_APPS)-apps git repository'
+	@echo '      Cloning $(GIT_NAME_APPS)-tools git repository'
 	$(call draw_line,)
-	if [ ! -e $(APPS_DIR)/.git ]; then \
-		git clone $(GITHUB)/$(GIT_NAME_APPS)/apps.git apps; \
+	if [ ! -e $(TOOLS_DIR)/.git ]; then \
+		git clone $(GITHUB)/$(GIT_NAME_APPS)/tools.git apps; \
 	fi
 
 $(FLASH_DIR):
@@ -368,7 +368,7 @@ $(FLASH_DIR):
 	fi
 	@echo ''
 
-PREQS  = $(APPS_DIR)
+PREQS  = $(TOOLS_DIR)
 
 ifeq ($(BOXARCH), sh4)
 PREQS += $(DRIVER_DIR)