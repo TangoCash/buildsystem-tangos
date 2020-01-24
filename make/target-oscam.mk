c1582b5c4fffb9d4b3e31166ba66cfcfc687d27e
TangoCash <eric@loxat.de>
2020-01-24 20:50:20 +0100

    fix last commit

--- make/target-oscam.mk
+++ make/target-oscam.mk
@@ -1,15 +1,7 @@
-$(D)/oscam-modern.do_prepare:
-	rm -rf $(SOURCE_DIR)/oscam-svn
-	rm -rf $(SOURCE_DIR)/oscam-svn.org
-	[ -d "$(ARCHIVE)/oscam-svn-modern" ] && \
-	(cd $(ARCHIVE)/oscam-svn-modern; svn up;); \
-	[ -d "$(ARCHIVE)/oscam-svn-modern" ] || \
-	svn checkout http://www.streamboard.tv/svn/oscam-addons/modern $(ARCHIVE)/oscam-svn-modern; \
-	cp -ra $(ARCHIVE)/oscam-svn-modern $(SOURCE_DIR)/oscam-svn;\
-	cp -ra $(SOURCE_DIR)/oscam-svn $(SOURCE_DIR)/oscam-svn.org;\
-	cd $(SOURCE_DIR)/oscam-svn; \
+OSCAM_CONFIG = \
 		./config.sh \
 			--disable all \
+			--enable readers \
 			--enable \
 				WEBIF \
 				CS_CACHEEX \
@@ -33,13 +25,11 @@ $(D)/oscam-modern.do_prepare:
 				\
 				CARDREADER_INTERNAL \
 				CARDREADER_PHOENIX \
-				CARDREADER_SC8IN1 \
-			--enable readers
-	touch $@
+				CARDREADER_SC8IN1
 
 $(D)/oscam-emu.do_prepare:
-	rm -rf $(SOURCE_DIR)/oscam-svn
-	rm -rf $(SOURCE_DIR)/oscam-svn.org
+	$(START_BUILD)
+	rm -rf $(SOURCE_DIR)/oscam-svn*
 	[ -d "$(ARCHIVE)/oscam-svn" ] && \
 	(cd $(ARCHIVE)/oscam-svn; svn up;); \
 	[ -d "$(ARCHIVE)/oscam-svn" ] || \
@@ -47,45 +37,20 @@ $(D)/oscam-emu.do_prepare:
 	cp -ra $(ARCHIVE)/oscam-svn $(SOURCE_DIR)/oscam-svn;\
 	cp -ra $(SOURCE_DIR)/oscam-svn $(SOURCE_DIR)/oscam-svn.org;\
 	cd $(SOURCE_DIR)/oscam-svn; \
-		wget https://github.com/oscam-emu/oscam-emu/raw/master/oscam-emu.patch; \
+		wget --progress=bar:force --no-check-certificate $(DOWNLOAD_SILENT_OPT) -t6 -T20 -O oscam-emu.patch https://github.com/oscam-emu/oscam-emu/raw/master/oscam-emu.patch; \
 		sed -i 's/SoftCam\.Key/oscam.keys/ig' oscam-emu.patch; \
 		sed -i 's/SoftCam_Key/oscam_keys/ig' oscam-emu.patch; \
-		patch -p0 < ./oscam-emu.patch; \
-		wget -O oscam.keys http://enigma.satupdate.net/SoftCam.txt ;\
-		./config.sh \
-			--disable all \
-			--enable \
-				WEBIF \
-				CS_CACHEEX \
-				HAVE_DVBAPI \
-				MODULE_MONITOR \
-				READ_SDT_CHARSETS \
-				WEBIF_JQUERY \
-				WEBIF_LIVELOG \
-				WITH_DEBUG \
-				WITH_EMU \
-				WITH_LB \
-				WITH_NEUTRINO \
-				WITH_EMU \
-				WITH_SOFTCAM \
-				\
-				MODULE_CAMD35 \
-				MODULE_CAMD35_TCP \
-				MODULE_CCCAM \
-				MODULE_CCCSHARE \
-				MODULE_CONSTCW \
-				MODULE_GBOX \
-				MODULE_NEWCAMD \
-				\
-				CARDREADER_INTERNAL \
-				CARDREADER_PHOENIX \
-				CARDREADER_SC8IN1 \
-			--enable readers
+		$(call apply_patches, $(SOURCE_DIR)/oscam-svn/oscam-emu.patch, 0); \
+		wget --progress=bar:force --no-check-certificate $(DOWNLOAD_SILENT_OPT) -t6 -T20 -O oscam.keys http://enigma.satupdate.net/SoftCam.txt ;\
+		$(OSCAM_CONFIG) \
+			WITH_EMU \
+			WITH_SOFTCAM \
+			$(SILENT_OPT)
 	touch $@
 
 $(D)/oscam.do_prepare:
-	rm -rf $(SOURCE_DIR)/oscam-svn
-	rm -rf $(SOURCE_DIR)/oscam-svn.org
+	$(START_BUILD)
+	rm -rf $(SOURCE_DIR)/oscam-svn*
 	[ -d "$(ARCHIVE)/oscam-svn" ] && \
 	(cd $(ARCHIVE)/oscam-svn; svn up;); \
 	[ -d "$(ARCHIVE)/oscam-svn" ] || \
@@ -93,45 +58,20 @@ $(D)/oscam.do_prepare:
 	cp -ra $(ARCHIVE)/oscam-svn $(SOURCE_DIR)/oscam-svn;\
 	cp -ra $(SOURCE_DIR)/oscam-svn $(SOURCE_DIR)/oscam-svn.org;\
 	cd $(SOURCE_DIR)/oscam-svn; \
-		./config.sh \
-			--disable all \
-			--enable \
-				WEBIF \
-				CS_CACHEEX \
-				HAVE_DVBAPI \
-				MODULE_MONITOR \
-				READ_SDT_CHARSETS \
-				WEBIF_JQUERY \
-				WEBIF_LIVELOG \
-				WITH_DEBUG \
-				WITH_EMU \
-				WITH_LB \
-				WITH_NEUTRINO \
-				\
-				MODULE_CAMD35 \
-				MODULE_CAMD35_TCP \
-				MODULE_CCCAM \
-				MODULE_CCCSHARE \
-				MODULE_CONSTCW \
-				MODULE_GBOX \
-				MODULE_NEWCAMD \
-				\
-				CARDREADER_INTERNAL \
-				CARDREADER_PHOENIX \
-				CARDREADER_SC8IN1 \
-			--enable readers
+		$(OSCAM_CONFIG) \
+			$(SILENT_OPT)
 	touch $@
 
 $(D)/oscam.do_compile:
 	cd $(SOURCE_DIR)/oscam-svn && \
 		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- CONF_DIR=/var/keys CC_OPTS=-Os
+		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- CONF_DIR=/var/keys
 	touch $@
 
-$(D)/oscam-emu.do_compile:
+$(D)/oscam-emu.do_compile: openssl
 	cd $(SOURCE_DIR)/oscam-svn && \
 		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" USE_LIBCRYPTO=1 CROSS=$(TARGET)- CONF_DIR=/var/keys VER=emu_svn
+		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_LIBCRYPTO=1 CONF_DIR=/var/keys VER=emu_svn
 	touch $@
 
 $(D)/oscam-ssl.do_compile: openssl
@@ -150,32 +90,30 @@ $(D)/oscam: bootstrap oscam.do_prepare oscam.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
 	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	touch $@
+	rm -rf $(SOURCE_DIR)/oscam-svn*
+	$(TOUCH)
 
-$(D)/oscam-ssl: bootstrap openssl oscam.do_prepare oscam-ssl.do_compile
+$(D)/oscam-ssl: bootstrap oscam.do_prepare oscam-ssl.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
 	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	touch $@
+	rm -rf $(SOURCE_DIR)/oscam-svn*
+	$(TOUCH)
 
-$(D)/oscam-libusb: bootstrap libusb oscam.do_prepare oscam-libusb.do_compile
+$(D)/oscam-libusb: bootstrap oscam.do_prepare oscam-libusb.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
 	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	touch $@
+	rm -rf $(SOURCE_DIR)/oscam-svn*
+	$(TOUCH)
 
-$(D)/oscam-modern: bootstrap oscam-modern.do_prepare oscam.do_compile
-	rm -rf $(TARGET_DIR)/../build_oscam
-	mkdir $(TARGET_DIR)/../build_oscam
-	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	touch $@
-
-$(D)/oscam-emu: bootstrap openssl oscam-emu.do_prepare oscam-emu.do_compile
+$(D)/oscam-emu: bootstrap oscam-emu.do_prepare oscam-emu.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
 	cp -pR $(SOURCE_DIR)/oscam-svn/oscam.keys $(TARGET_DIR)/../build_oscam/
 	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	touch $@
+	rm -rf $(SOURCE_DIR)/oscam-svn*
+	$(TOUCH)
 
 oscam-clean:
 	rm -f $(D)/oscam
@@ -184,8 +122,12 @@ oscam-clean:
 
 oscam-distclean:
 	rm -f $(D)/oscam
+	rm -f $(D)/oscam-emu
+	rm -f $(D)/oscam-libusb
 	rm -f $(D)/oscam.do_compile
+	rm -f $(D)/oscam-emu.do_compile
 	rm -f $(D)/oscam-libusb.do_compile
 	rm -f $(D)/oscam.do_prepare
-	rm -f $(D)/oscam-modern.do_prepare
+	rm -f $(D)/oscam-emu.do_prepare
+	rm -f $(D)/oscam-libusb.do_prepare
 
