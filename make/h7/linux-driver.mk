e7cfee1546f899ca5edc626788c6925cad79dcf1
TangoCash <eric@loxat.de>
2021-03-14 15:45:41 +0100

    fix typo

--- make/h7/linux-driver.mk
+++ make/h7/linux-driver.mk
@@ -28,7 +28,7 @@ $(D)/driver: $(ARCHIVE)/$(DRIVER_SRC) $(D)/bootstrap $(D)/kernel
 	install -d $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra
 	unzip -o $(ARCHIVE)/$(DRIVER_SRC) -d $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra
 	ls $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra | sed s/.ko//g > $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/modules.default
-	sed -i "s/_4/_4 boxmode=\$BOXMODE/g" $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/modules.default
+	sed -i "s/_4/_4 boxmode=\$$BOXMODE/g" $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/modules.default
 	$(MAKE) install-v3ddriver
 	$(MAKE) install-v3ddriver-header
 	$(TOUCH)
