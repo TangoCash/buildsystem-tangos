3f4db0c6df5098c5729fa4dab14702326ba7c752
TangoCash <eric@loxat.de>
2022-08-21 20:00:02 +0200

    update osmio4k(+)

--- make/osmio4kplus/linux-driver.mk
+++ make/osmio4kplus/linux-driver.mk
@@ -1,7 +1,7 @@
 #
 # driver
 #
-DRIVER_DATE = 20211103
+DRIVER_DATE = 20211228
 DRIVER_VER  = 5.15.0-$(DRIVER_DATE)
 DRIVER_SRC  = osmio4kplus-drivers-$(DRIVER_VER).zip
 
@@ -24,6 +24,7 @@ $(D)/driver: $(ARCHIVE)/$(DRIVER_SRC) $(D)/bootstrap $(D)/kernel
 	$(START_BUILD)
 	install -d $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra
 	unzip -o $(ARCHIVE)/$(DRIVER_SRC) -d $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra
+	rm -f $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/modules.default
 	for i in brcmstb-osmio4kplus brcmstb-decoder ci si2183 avl6862 avl6261; do \
 		echo $$i >> $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/modules.default; \
 	done
@@ -43,10 +44,10 @@ $(D)/install-v3ddriver: $(ARCHIVE)/$(LIBGLES_SRC)
 #
 # wlan-qcom osmio4k | osmio4kplus
 #
-WLAN_QCOM_VER    = 4.5.25.46
+WLAN_QCOM_VER    = 4.5.25.55
 WLAN_QCOM_DIR    = qcacld-2.0-$(WLAN_QCOM_VER)
 WLAN_QCOM_SOURCE = qcacld-2.0-$(WLAN_QCOM_VER).tar.gz
-WLAN_QCOM_URL    = https://www.codeaurora.org/cgit/external/wlan/qcacld-2.0/snapshot
+WLAN_QCOM_URL    = https://source.codeaurora.org/external/wlan/qcacld-2.0/snapshot
 
 $(ARCHIVE)/$(WLAN_QCOM_SOURCE):
 	$(DOWNLOAD) $(WLAN_QCOM_URL)/$(WLAN_QCOM_SOURCE)
@@ -62,13 +63,14 @@ $(D)/wlan-qcom: $(D)/bootstrap $(D)/kernel $(D)/wlan-qcom-firmware $(ARCHIVE)/$(
 		$(call apply_patches, $(WLAN_QCOM_PATCH)); \
 		$(MAKE) KERNEL_SRC=$(KERNEL_DIR) ARCH=arm CROSS_COMPILE=$(TARGET)- CROSS_COMPILE_COMPAT=$(TARGET)- all; \
 	install -m 644 wlan.ko $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra
+	echo wlan >> $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/modules.default
 	$(REMOVE)/$(WLAN_QCOM_DIR)
 	$(TOUCH)
 
 #
 # wlan-qcom-firmware
 #
-WLAN_QCOM_FIRMWARE_VER    = qca6174
+WLAN_QCOM_FIRMWARE_VER    = qca6174_v2
 WLAN_QCOM_FIRMWARE_DIR    = firmware-$(WLAN_QCOM_FIRMWARE_VER)
 WLAN_QCOM_FIRMWARE_SOURCE = firmware-$(WLAN_QCOM_FIRMWARE_VER).zip
 WLAN_QCOM_FIRMWARE_URL    = http://source.mynonpublic.com/edision
@@ -81,6 +83,8 @@ $(D)/wlan-qcom-firmware: $(D)/bootstrap $(ARCHIVE)/$(WLAN_QCOM_FIRMWARE_SOURCE)
 	$(REMOVE)/$(WLAN_QCOM_FIRMWARE_DIR)
 	unzip -o $(ARCHIVE)/$(WLAN_QCOM_FIRMWARE_SOURCE) -d $(BUILD_TMP)/$(WLAN_QCOM_FIRMWARE_DIR)
 	$(CHDIR)/$(WLAN_QCOM_FIRMWARE_DIR); \
+		install -d $(TARGET_DIR)/lib/firmware; \
+		install -m 644 btfw32.tlv $(TARGET_DIR)/lib/firmware/btfw32.tlv; \
 		install -d $(TARGET_DIR)/lib/firmware/ath10k/QCA6174/hw3.0; \
 		install -m 644 board.bin $(TARGET_DIR)/lib/firmware/ath10k/QCA6174/hw3.0/board.bin; \
 		install -m 644 firmware-4.bin $(TARGET_DIR)/lib/firmware/ath10k/QCA6174/hw3.0/firmware-4.bin; \
@@ -91,6 +95,8 @@ $(D)/wlan-qcom-firmware: $(D)/bootstrap $(ARCHIVE)/$(WLAN_QCOM_FIRMWARE_SOURCE)
 		install -m 644 utf30.bin $(TARGET_DIR)/lib/firmware/utf30.bin; \
 		install -m 644 wlan/cfg.dat $(TARGET_DIR)/lib/firmware/wlan/cfg.dat; \
 		install -m 644 wlan/qcom_cfg.ini $(TARGET_DIR)/lib/firmware/wlan/qcom_cfg.ini; \
-		install -m 644 btfw32.tlv $(TARGET_DIR)/lib/firmware/btfw32.tlv
+		install -d $(TARGET_DIR)/lib/firmware/qca;\
+		install -m 644 nvm_00440302.bin $(TARGET_DIR)/lib/firmware/qca/nvm_00440302.bin; \
+		install -m 644 rampatch_00440302.bin $(TARGET_DIR)/lib/firmware/qca/rampatch_00440302.bin
 	$(REMOVE)/$(WLAN_QCOM_FIRMWARE_DIR)
 	$(TOUCH)
