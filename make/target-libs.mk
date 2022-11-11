0bf1f68f0080ea097d449b36664d366f9800bd69
TangoCash <eric@loxat.de>
2022-11-11 12:23:13 +0100

    add lcd4linux internal display support for vuultimo4k

--- make/target-libs.mk
+++ make/target-libs.mk
@@ -1732,7 +1732,7 @@ $(D)/lcd4linux: $(D)/bootstrap $(D)/libusb_compat $(D)/gd $(D)/libusb $(D)/libdp
 		$(BUILDENV) ./bootstrap $(SILENT_OPT); \
 		$(BUILDENV) ./configure $(CONFIGURE_OPTS) $(SILENT_OPT) \
 			--prefix=/usr \
-			--with-drivers='DPF,SamsungSPF,PNG' \
+			--with-drivers='DPF,SamsungSPF,PNG,$(LCD4LINUX_EXTRA_DRIVER)' \
 			--with-plugins='all,!apm,!asterisk,!dbus,!dvb,!gps,!hddtemp,!huawei,!imon,!isdn,!kvv,!mpd,!mpris_dbus,!mysql,!pop3,!ppp,!python,!qnaplog,!raspi,!sample,!seti,!w1retap,!wireless,!xmms' \
 			--without-ncurses \
 		; \
