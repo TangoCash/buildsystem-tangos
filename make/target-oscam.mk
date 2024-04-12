ea1852c7137c825dffb591f40eb902d79c0c5a70
TangoCash <eric@loxat.de>
2024-04-12 09:42:55 +0200

    oscam: svn->git

--- make/target-oscam.mk
+++ make/target-oscam.mk
@@ -29,20 +29,17 @@ OSCAM_CONFIG = \
 				CARDREADER_PHOENIX \
 				CARDREADER_SC8IN1
 
+OSCAM_GIT = https://git.streamboard.tv/common/oscam.git
+
 $(D)/oscam-emu.do_prepare:
 	$(START_BUILD)
-	rm -rf $(SOURCE_DIR)/oscam-svn*
-	[ -d "$(ARCHIVE)/oscam-svn" ] && \
-	(cd $(ARCHIVE)/oscam-svn; svn up;); \
-	[ -d "$(ARCHIVE)/oscam-svn" ] || \
-	svn checkout http://www.streamboard.tv/svn/oscam/trunk $(ARCHIVE)/oscam-svn; \
-	cp -ra $(ARCHIVE)/oscam-svn $(SOURCE_DIR)/oscam-svn;\
-	cp -ra $(SOURCE_DIR)/oscam-svn $(SOURCE_DIR)/oscam-svn.org;\
-	cd $(SOURCE_DIR)/oscam-svn; \
+	$(REMOVE)/$(PKG_NAME)
+	$(call update_git, $(OSCAM_GIT))
+	$(CHDIR)/$(PKG_NAME); \
 		wget --progress=bar:force --no-check-certificate $(DOWNLOAD_SILENT_OPT) -t6 -T20 -O oscam-emu.patch $(GITHUB)/oscam-emu/oscam-emu/raw/master/oscam-emu.patch; \
 		sed -i 's/SoftCam\.Key/oscam.keys/ig' oscam-emu.patch; \
 		sed -i 's/SoftCam_Key/oscam_keys/ig' oscam-emu.patch; \
-		$(call apply_patches, $(SOURCE_DIR)/oscam-svn/oscam-emu.patch, 0); \
+		$(call apply_patches, $(BUILD_TMP)/$(PKG_NAME)/oscam-emu.patch, 0); \
 		$(call apply_patches, $(LOCAL_OSCAM_EMU_PATCHES)); \
 		wget --progress=bar:force --no-check-certificate $(DOWNLOAD_SILENT_OPT) -t6 -T20 -O oscam.keys http://enigma.satupdate.net/SoftCam.txt ;\
 		$(OSCAM_CONFIG) \
@@ -53,113 +50,78 @@ $(D)/oscam-emu.do_prepare:
 
 $(D)/oscam.do_prepare:
 	$(START_BUILD)
-	rm -rf $(SOURCE_DIR)/oscam-svn*
-	[ -d "$(ARCHIVE)/oscam-svn" ] && \
-	(cd $(ARCHIVE)/oscam-svn; svn up;); \
-	[ -d "$(ARCHIVE)/oscam-svn" ] || \
-	svn checkout http://www.streamboard.tv/svn/oscam/trunk $(ARCHIVE)/oscam-svn; \
-	cp -ra $(ARCHIVE)/oscam-svn $(SOURCE_DIR)/oscam-svn;\
-	cp -ra $(SOURCE_DIR)/oscam-svn $(SOURCE_DIR)/oscam-svn.org;\
-	cd $(SOURCE_DIR)/oscam-svn; \
+	$(REMOVE)/$(PKG_NAME)
+	$(call update_git, $(OSCAM_GIT))
+	$(CHDIR)/$(PKG_NAME); \
 		$(call apply_patches, $(LOCAL_OSCAM_PATCHES)); \
 		$(OSCAM_CONFIG) \
 			$(SILENT_OPT)
 	touch $@
 
 $(D)/oscam.do_compile:
-	cd $(SOURCE_DIR)/oscam-svn && \
+	cd $(BUILD_TMP)/oscam && \
 		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- CONF_DIR=/var/keys
+		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- CONF_DIR=/var/keys VER=static_ LIBDVBCSA_LIB=$(TARGET_LIB_DIR)/libdvbcsa.a
 	touch $@
 
 $(D)/oscam-emu.do_compile: openssl
-	cd $(SOURCE_DIR)/oscam-svn && \
+	cd $(BUILD_TMP)/oscam && \
 		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_LIBCRYPTO=1 CONF_DIR=/var/keys VER=emu_svn
+		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_LIBCRYPTO=1 CONF_DIR=/var/keys VER=static_emu_ LIBDVBCSA_LIB=$(TARGET_LIB_DIR)/libdvbcsa.a
 	touch $@
 
 $(D)/oscam-ssl.do_compile: openssl
-	cd $(SOURCE_DIR)/oscam-svn && \
+	cd $(BUILD_TMP)/oscam && \
 		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_SSL=1 CONF_DIR=/var/keys VER=ssl_svn
+		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_SSL=1 CONF_DIR=/var/keys VER=static_ssl_ LIBDVBCSA_LIB=$(TARGET_LIB_DIR)/libdvbcsa.a
 	touch $@
 
 $(D)/oscam-libusb.do_compile: libusb
-	cd $(SOURCE_DIR)/oscam-svn && \
+	cd $(BUILD_TMP)/oscam && \
 		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_LIBUSB=1 CONF_DIR=/var/keys VER=libusb_svn
-	touch $@
-
-$(D)/oscam-static.do_compile:
-	cd $(SOURCE_DIR)/oscam-svn && \
-		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- CONF_DIR=/var/keys LIBDVBCSA_LIB=$(TARGET_LIB_DIR)/libdvbcsa.a
-	touch $@
-
-$(D)/oscam-emu-static.do_compile: openssl
-	cd $(SOURCE_DIR)/oscam-svn && \
-		$(BUILDENV) \
-		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_LIBCRYPTO=1 CONF_DIR=/var/keys VER=emu_svn LIBDVBCSA_LIB=$(TARGET_LIB_DIR)/libdvbcsa.a
+		$(MAKE) EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" CROSS=$(TARGET)- USE_LIBUSB=1 CONF_DIR=/var/keys VER=static_libusb_ LIBDVBCSA_LIB=$(TARGET_LIB_DIR)/libdvbcsa.a
 	touch $@
 
 $(D)/oscam: bootstrap libdvbcsa oscam.do_prepare oscam.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
-	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	rm -rf $(SOURCE_DIR)/oscam-svn*
+	cp -pR $(BUILD_TMP)/oscam/Distribution/* $(TARGET_DIR)/../build_oscam/
+	rm -rf $(BUILD_TMP)/oscam*
 	$(TOUCH)
 
-$(D)/oscam-ssl: bootstrap libdvbcsa oscam.do_prepare oscam-ssl.do_compile
+$(D)/oscam-ssl: bootstrap libdvbcsa openssl oscam.do_prepare oscam-ssl.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
-	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	rm -rf $(SOURCE_DIR)/oscam-svn*
+	cp -pR $(BUILD_TMP)/oscam/Distribution/* $(TARGET_DIR)/../build_oscam/
+	rm -rf $(BUILD_TMP)/oscam*
 	$(TOUCH)
 
 $(D)/oscam-libusb: bootstrap libdvbcsa oscam.do_prepare oscam-libusb.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
-	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	rm -rf $(SOURCE_DIR)/oscam-svn*
+	cp -pR $(BUILD_TMP)/oscam/Distribution/* $(TARGET_DIR)/../build_oscam/
+	rm -rf $(BUILD_TMP)/oscam*
 	$(TOUCH)
 
-$(D)/oscam-emu: bootstrap oscam-emu.do_prepare oscam-emu.do_compile
+$(D)/oscam-emu: bootstrap libdvbcsa oscam-emu.do_prepare oscam-emu.do_compile
 	rm -rf $(TARGET_DIR)/../build_oscam
 	mkdir $(TARGET_DIR)/../build_oscam
-	cp -pR $(SOURCE_DIR)/oscam-svn/oscam.keys $(TARGET_DIR)/../build_oscam/
-	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	rm -rf $(SOURCE_DIR)/oscam-svn*
-	$(TOUCH)
-
-$(D)/oscam-emu-static: bootstrap oscam-emu.do_prepare oscam-emu-static.do_compile
-	rm -rf $(TARGET_DIR)/../build_oscam
-	mkdir $(TARGET_DIR)/../build_oscam
-	cp -pR $(SOURCE_DIR)/oscam-svn/oscam.keys $(TARGET_DIR)/../build_oscam/
-	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	rm -rf $(SOURCE_DIR)/oscam-svn*
-	$(TOUCH)
-
-$(D)/oscam-static: bootstrap libdvbcsa oscam.do_prepare oscam-static.do_compile
-	rm -rf $(TARGET_DIR)/../build_oscam
-	mkdir $(TARGET_DIR)/../build_oscam
-	cp -pR $(SOURCE_DIR)/oscam-svn/Distribution/* $(TARGET_DIR)/../build_oscam/
-	rm -rf $(SOURCE_DIR)/oscam-svn*
+	cp -pR $(BUILD_TMP)/oscam/oscam.keys $(TARGET_DIR)/../build_oscam/
+	cp -pR $(BUILD_TMP)/oscam/Distribution/* $(TARGET_DIR)/../build_oscam/
+	rm -rf $(BUILD_TMP)/oscam*
 	$(TOUCH)
 
 oscam-clean:
 	rm -f $(D)/oscam
-	cd $(SOURCE_DIR)/oscam-svn && \
+	cd $(BUILD_TMP)/oscam && \
 		$(MAKE) distclean
 
 oscam-distclean:
 	rm -f $(D)/oscam
 	rm -f $(D)/oscam-emu
+	rm -f $(D)/oscam-ssl
 	rm -f $(D)/oscam-libusb
-	rm -f $(D)/oscam.do_compile
-	rm -f $(D)/oscam-static.do_compile
-	rm -f $(D)/oscam-emu.do_compile
-	rm -f $(D)/oscam-emu-static.do_compile
-	rm -f $(D)/oscam-libusb.do_compile
-	rm -f $(D)/oscam.do_prepare
-	rm -f $(D)/oscam-emu.do_prepare
-	rm -f $(D)/oscam-libusb.do_prepare
+	rm -f $(D)/oscam.*
+	rm -f $(D)/oscam-emu.*
+	rm -f $(D)/oscam-ssl.*
+	rm -f $(D)/oscam-libusb.*
